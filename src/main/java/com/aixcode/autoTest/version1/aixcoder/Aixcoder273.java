2b29192f34747978696b490a4df79e75c8e1295d
liuyongqiang <liuyq1991@126.com>
2022-06-09 17:37:03 +0800

    删除文件

--- src/main/java/com/aixcode/autoTest/version1/aixcoder/Aixcoder273.java
+++
@@ -1,9 +0,0 @@
-package com.aixcode.autoTest.version1.aixcoder;
-
-public class Aixcoder273 {
-    public static void checkRange(int value, int min, int max) {
-        if ((value < min) || (value > max)) {
-            throw new IllegalArgumentException("Value is out of range:" + value);
-        }
-    }
-}
--- src/main/java/com/aixcode/autoTest/version1/aixcoder/Aixcoder394.java
+++
@@ -1,8 +0,0 @@
-package com.aixcode.autoTest.version1.aixcoder;
-
-public class Aixcoder394 {
-//    public void print(float x, OutputStream out) {
-//        out.print(x);
-//        out.flush();
-//    }
-}
