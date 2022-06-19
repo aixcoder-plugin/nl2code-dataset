85d761ac993760c2a72d64b37851120d7a1746f8
刘永强 <liuyq1991@126.com>
2022-06-20 00:15:48 +0800

    evaluation

--- src/main/java/com/aixcode/autoTest/aixcoderV2/AixcoderOld137.java
+++
@@ -1,8 +0,0 @@
-package com.aixcode.autoTest.aixcoderV2;
-
-import java.util.*;
-import java.io.*;
-import com.aixcode.autoTest.GenerateMethodBase;
-public class AixcoderOld137 extends GenerateMethodBase {
-        
-    }
--- src/main/java/com/aixcode/autoTest/aixcoderV2/Test.java
+++
@@ -1,6 +0,0 @@
-package com.aixcode.autoTest.aixcoderV2;
-
-import com.aixcode.autoTest.GenerateMethodBase;
-
-public class Test extends GenerateMethodBase {
-}
