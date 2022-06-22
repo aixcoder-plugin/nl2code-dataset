5c26790127113b1cb75f56a542963de713298e43
miaoxiaowei1112 <miaoxiaowei@nnthink.com>
2022-06-22 17:59:59 +0800

    modify test cases of 120-130

--- src/main/java/com/aixcode/autoTest/evaluation/Evaluation124.java
+++ src/main/java/com/aixcode/autoTest/evaluation/Evaluation124.java
@@ -16,43 +16,50 @@ public class Evaluation124 extends AbstractBaseEvaluation {
     @Override
     public int[] evaluation() {
         int pass_count = 0;
-        List<String> list1 = null;
-        List<String> list2 = new ArrayList<>(){{
-            add("1");
+        List<String> lstNull = null;
+        List<String> lstEmpty = new ArrayList<>();
+        List<String> lstNormal = new ArrayList<>(){{
+            add("str1");
+            add("str2");
+            add("str3");
+            add("str4");
         }};
-        List<String> list3 = new ArrayList<>(){{
-            add("1");
-            add("2");
+
+        Map<Integer,List<String>> params1 = new HashMap<>(){{
+            put(1, lstNull); //List is null
+            put(2, lstEmpty);//List is empty, not null
+            put(3, lstNormal);// List is normal, and String is empty
+            put(4, lstNormal);// List is normal, and String is null
+            put(5, lstNormal);// List is normal, and String is normal
         }};
-        List<String> list4 = new ArrayList<>(){{
-            add("1");
-            add("2");
-            add("3");
-        }};
-        List<String> list5 = new ArrayList<>(){{
-            add("1");
-            add("2");
-            add("3");
-            add("4");
-        }};
-        Map<List<String>, Integer> map = new HashMap<>(){{
-            put(list1,1);
-            put(list2, 2);
-            put(list3, 3);
-            put(list4, 4);
-            put(list5, 5);
-        }};
-        for (Map.Entry<List<String>, Integer> entry : map.entrySet()){
+        Map<Integer,String> params2 = new HashMap<>(){
+            {
+                put(1, "normal"); //List is null
+                put(2, "normal");//List is empty, not null
+                put(3, "");// List is normal, and String is empty
+                put(4, null);// List is normal, and String is null
+                put(5, "normal");// List is normal, and String is normal
+            }
+        };
+
+        for (Map.Entry<Integer, List<String>> entry : params1.entrySet()){
             try {
-                List<String> list=  entry.getKey();
-                solution.append(list,"23");
-                if (null != list && list.size() == entry.getValue()){
-                    pass_count++;
+                List<String> list =  entry.getValue();
+                Integer key = entry.getKey();
+                String paramString = params2.get(key);
+                solution.append(list, paramString);
+                if (null != list){
+                    if((paramString == null && list.get(list.size() - 1) == null) || (paramString != null)&&paramString.equals(list.get(list.size() - 1))){
+                        pass_count++;
+                    }
+                }else {
+                    if(params1.get(key) == null){
+                        pass_count++;
+                    }
                 }
             }catch (Exception e) {
-
             }
         }
-        return new int[]{pass_count,map.size()};
+        return new int[]{pass_count,params1.size()};
     }
 }
